6b619f6c14fd92736e19e6f6459520cc4d04be9f
jung2uncan <jung2uncan@gmail.com>
2021-12-22 17:45:16 +0900

    [2021.12.22][jung2uncan][MyBatis를 활용한 CRUD 구현하기]

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -2,11 +2,26 @@ package org.zerock.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
 import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
 	
-	@Select("SELECT * FROM tbl_board WHERE bno > 0")
+	//게시글 전체 조회
+	//@Select("SELECT * FROM tbl_board WHERE bno > 0")
 	public List<BoardVO> getList();
+	
+	//게시글 삽입 : insert 처리 후 생성된 PK값을 알 필요 없는 경우
+	public void insert(BoardVO board);
+	
+	//게시글 삽입 : insert 처리 후 생성된 PK값을 알아야 할 경우
+	public void insertSelectKey(BoardVO board);
+	
+	//게시글 읽기 : 게시글 번호를 통해서 1개의 글 읽어오기
+	public BoardVO read(Long bno);
+	
+	//게시글 삭제 : 특정 게시글을 삭제하기
+	public int delete(Long bno);
+	
+	//게시글 수정 : 특정 게시글 수정하기
+	public int update(BoardVO board);
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -22,4 +23,55 @@ public class BoardMapperTests {
 		mapper.getList().forEach(board -> log.info(board));
 	}
 	
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 테스트");
+		board.setContent("새로 작성하는 내용 테스트");
+		board.setWriter("newbie");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 테스트 Select Key");
+		board.setContent("새로 작성하는 내용 테스트 Select Key");
+		board.setWriter("newbie");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testRead() {
+		//존재하는 게시글 번호로 테스트 진행 필수
+		BoardVO board = mapper.read(1L);
+		
+		log.info(board);
+	}
+	
+	@Test 
+	public void testDelete() {
+		//존재하는 게시글 번호로 테스트 진행 필수
+		//삭제하고자하는 게시글이 있다면 1, 없으면 0 반환
+		log.info("DELETE COUNT: " + mapper.delete(22L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		board.setBno(29L);
+		board.setTitle("수정한 글 입니다.");
+		board.setContent("수정하는 내용 테스트");
+		board.setWriter("user00");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT: " + count);
+	}
+
 }
